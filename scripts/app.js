bef909b41104fffb3de804af0f66e30754069b24
jsans24 <jsans24@gmail.com>
2020-09-10 22:19:13 -0700

    Commit 3 - Excited Kittens answered

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,19 @@
 // Easy Going
-// 1.
+// 1. Write a for loop that will log the numbers 1 through 20.
 for (i = 1; i < 21; i++) console.log(i)
+
+// Get Even
+// 1. Write a for loop that will log only the even numbers in 0 through 200.
+for (i = 0; i < 201; i+=2) console.log(i)
+
+// Excited Kitten
+// 1. Write code that logs "Love me, pet me! HSSSSSS!" 20 times.
+// 2. For every even number in your loop, log "...human...why you taking pictures of me?...", "...the catnip made me do it...", or "...why does the red dot always get away..." at random.
+const catSays = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."]
+function getRandomInt(max){
+    return Math.floor(Math.random() * Math.floor(max))
+}
+for (i = 1; i < 21; i++) {
+    if (i % 2 === 0) console.log(catSays[getRandomInt(3)])
+    else console.log('Love me, pet me! HSSSSSS!')
+}
